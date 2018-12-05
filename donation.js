727c63a071efbee72ff6935b0af71067a2b33811
sophie <sophie.engel1202@gmail.com>
2018-12-05 10:47:54 +0100

    js ændring

--- donation.js
+++ donation.js
@@ -3,27 +3,26 @@ window.addEventListener("load", clickedDonation);
 const endpoint1 = "https://5c0654d5c16e1200139479ba.mockapi.io/donationer";
 
 const e = {
-    ID: "1",
-    Amount: "500"
+  userID: 1,
+  what: [],
+  amount: 500,
+  createdAt: Date.now()
 };
 
-
-function clickedDonation(){
-    document.querySelector("#donerknap").addEventListener("click", donerClicked);
-
+function clickedDonation() {
+  document.querySelector("#donerknap").addEventListener("click", donerClicked);
 }
 
-function donerClicked(){  
-    console.log(e) 
-    fetch(endpoint1, {
-        method: "post",
-        body: JSON.stringify(e),
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json"
-        }
-    })
-.then(res => res.json())
-.then(d => {});
+function donerClicked() {
+  console.log(e);
+  fetch(endpoint1, {
+    method: "post",
+    body: JSON.stringify(e),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    }
+  })
+    .then(res => res.json())
+    .then(d => {});
 }
-
--- main.js
+++ main.js
@@ -1,90 +1,72 @@
 window.addEventListener("load", initList);
 
 const a = {
-    ID: "1",
-    name: "Magnus"
-  };
-  
+  name: "Magnus"
+};
+
 const b = {
-    ID: "2",
-    name: "Sophie"
-  };
+  name: "Sophie"
+};
 
 const c = {
-    ID: "3",
-    name: "Ib"
-  };
+  name: "Ib"
+};
 
 const endpoint = "https://5c0654d5c16e1200139479ba.mockapi.io/users";
 
-  fetch(endpoint, {
-    method: "post",
-    body: JSON.stringify(a),
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json"
-      
-    }
-    
-  })
+fetch(endpoint, {
+  method: "post",
+  body: JSON.stringify(a),
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
 
-  fetch(endpoint, {
-    method: "post",
-    body: JSON.stringify(b),
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json"
-      
-    }
-    
-  })
-  fetch(endpoint, {
-    method: "post",
-    body: JSON.stringify(c),
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json"
-      
-    }
-    
-  })
+fetch(endpoint, {
+  method: "post",
+  body: JSON.stringify(b),
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+fetch(endpoint, {
+  method: "post",
+  body: JSON.stringify(c),
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+})
+  .then(res => res.json())
+  .then(d => {});
 
+function initList() {
+  console.log("Init list");
+
+  listUsers();
+}
+
+function listUsers() {
+  fetch(endpoint, {
+    method: "get",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    }
+  })
     .then(res => res.json())
-    .then(d => {});
-    
-    function initList() {
-        console.log("Init list");
-    
-    
-        listUsers();
-    }
-    
-    function listUsers() {
-    
-        
-    
-    fetch(endpoint, {
-        method: "get",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json"
-        }
-      })
-        .then(res => res.json())
-        .then(data => {
-          
-            data.forEach( createUserInfo );
-    
-    
-        });
-    
-    }
-    
-    function createUserInfo( userData ) {
-        const clone = document.querySelector("#userinfo").content.cloneNode(true);
-    
-        clone.querySelector("[data-username]").textContent = userData.name;
-        clone.querySelector("[data-id]").dataset.userid = userData.id;
-    
-        document.querySelector("#userlist").appendChild(clone);
-    }
+    .then(data => {
+      data.forEach(createUserInfo);
+    });
+}
+
+function createUserInfo(userData) {
+  const clone = document.querySelector("#userinfo").content.cloneNode(true);
+
+  clone.querySelector("[data-username]").textContent = userData.name;
+  clone.querySelector("[data-id]").dataset.userid = userData.id;
+
+  document.querySelector("#userlist").appendChild(clone);
+}
